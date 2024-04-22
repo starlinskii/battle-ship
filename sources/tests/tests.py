7fa2542104ad46e48418c79cd789a8d9e0a4c4e1
starlinskii <starlinskiy@gmail.com~>
2024-04-22 17:57:12 +0300

    add tests

---
+++ sources/tests/tests.py
@@ -0,0 +1,23 @@
+def check_field():
+    # test case 1
+    # 0 0 0 1
+    # 0 0 3 1
+    # 0 0 6 1
+    # 0 2 0 1
+    # 0 2 2 1
+    # 0 2 4 1
+    # 0 2 6 1
+    # 0 4 0 1
+    # 0 6 0 1
+    # 1
+    assert 1 == 1
+
+def check_shots():
+    # test case 2
+    # 0 0
+    # 0 0
+    # 0 0
+    # ... till the computer's win
+    assert 1 == 1
+
+
